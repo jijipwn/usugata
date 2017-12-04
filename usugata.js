bff7b998e7a4f077defa718eadab23c885197f3e
jiji dev <33302635+jijipwn@users.noreply.github.com>
2017-12-04 19:07:10 +1100

    Updated to 1.1

--- usugata.js
+++ usugata.js
@@ -2,15 +2,11 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = ".";
 
-//to use download node.js, then open cmd in the folder usugata.js is in, type "npm i --save discord.js" after thats done
-//type "node usugata.js"
-
 client.on('ready', () => {
   console.log('usugata is alive');
   client.user.setStatus('dnd')
-  client.user.setGame('with my DS')
+  client.user.setGame('PictoChat 📓')
 });
-
 
 client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
@@ -20,46 +16,80 @@ client.on('message', message => {
   switch (args[0].toLowerCase()) {
      case "info":
 	   var embed = new Discord.RichEmbed()
-          .addField("Info", "I'm usugata, I'm here to help you on the /r/NDShacks Discord")
+      .addField("Info <:usugata:387119258183794698>", "I'm usugata, I'm here to help you on the /r/NDShacks Discord")
 		  .setColor(0x7e8a9e)
 		  .setFooter("usugata");
          message.channel.send(embed);
+         message.react('387119258183794698')
      break;
      case "guide":
 	   var embed = new Discord.RichEmbed()
-          .addField("Guide", "[click here!](https://dsiguide.me)")
+          .addField("Guide <:usugata:387119258183794698>", "The DSi Guide can be found [here](https://dsiguide.me)")
           .setColor(0x7e8a9e)
 		  .setFooter("usugata");
          message.channel.send(embed);
+         message.react('387119258183794698')
+      break;
+      case "cheats":
+      var embed = new Discord.RichEmbed()
+         .addField("Guide <:usugata:387119258183794698>", "Here is the approved Cheat Database's by DeadSkullzJr [Click Here!](https://gbatemp.net/threads/deadskullzjrs-flashcart-cheat-databases.488711/)")
+         .setColor(0x7e8a9e)
+         .setFooter("usugata");
+             message.channel.send(embed);
+             message.react('387119258183794698')
      break;
      case "help":
 	   var embed = new Discord.RichEmbed()
-          .addField("usugata", "Here are my Commands:", true)
+          .addField("usugata <:usugata:387119258183794698>", "Here are my Commands:", true)
           .addField("Prefix", "My Prefix is **.**", true)
-          .addField("Info", "Gives you info on the bot",)
-          .addField("Guide", "Gives you a link to the DSi Guide",)
-          .addField("Help", "Tells you this Message",)
-          .addField("Reddit", "Gives you a link to the subreddit",)
+          .addField("Ping", "Pong! 🏓", true)
+          .addField("Info", "Gives you info on the bot", true)
+          .addField("Guide", "Gives you a link to the DSi Guide", true)
+          .addField("Help", "Tells you this Message", true)
+          .addField("Reddit", "Gives you a link to the subreddit", true)
+          .addField("td", "Gives a link to ThreeDeus, a helpful downloading tool", true)
+          .addField("cheats", "Gives you a link to the cheat databases run by DeadSkullzJr", true)
           .setColor(0x7e8a9e)
           .setThumbnail(message.client.user.avatarURL)
-		  .setFooter("usugata");
-         message.channel.send(embed);
+		      .setFooter("usugata")
+      var embed2 = new Discord.RichEmbed()
+          .addField("usugata <:usugata:387119258183794698>", "Help is on its way!")
+          .setColor(0x7e8a9e)
+     		  .setFooter("usugata")
+         message.author.send(embed)
+         message.channel.send(embed2)
+         message.react('387119258183794698')
      break;
      case "reddit":
 	   var embed = new Discord.RichEmbed()
-          .addField("Reddit", "[Here!](https://reddit.com/r/NDShacks)")
-		  .setColor(0x7e8a9e)
-		  .setFooter("usugata");
+          .addField("Reddit <:usugata:387119258183794698>", "You can enter the subreddit [Here!](https://reddit.com/r/NDShacks)")
+		      .setColor(0x7e8a9e)
+		      .setFooter("usugata");
          message.channel.send(embed);
+         message.react('387119258183794698')
+    break;
+    case "td":
+    var embed = new Discord.RichEmbed()
+        .addField("ThreeDeus <:usugata:387119258183794698>", "ThreeDeus is a helpful tool for downloading files")
+        .addField("DOWNLOAD:", "ThreeDeus can be downloaded [here.](https://github.com/jijipwn/ThreeDeus/releases/latest)")
+        .setColor(0x7e8a9e)
+        .setFooter("usugata");
+        message.channel.send(embed);
+        message.react('387119258183794698')
+     break;
+     case "ping":
+     var embed = new Discord.RichEmbed()
+          .addField("usugata <:usugata:387119258183794698>", ":ping_pong: Pong at " + (Date.now() - message.createdTimestamp) + " ms!")
+          .setColor(0x7e8a9e)
+          .setFooter("usugata");
+         message.channel.send(embed);
+         message.react('387119258183794698')
      break;
      default:
-	  var embed = new Discord.RichEmbed()
-		  .addField("usugata", "The command you've typed does not exist. try .help!")
-		  .setColor(0x7e8a9e)
-		  .setFooter("usugata");
-		 message.channel.send(embed);
+     message.react('❎')
   }
 });
 
+// 🇳
 
-client.login('add your own bot token');
+client.login('SuperSecretBotTokenHere');
